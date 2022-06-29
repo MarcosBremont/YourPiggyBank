0512d8d4c4aaaa6885b468efc6dc8c1c15f8e09a
Marcos Bremont <bremont123@hotmail.com>
2022-06-29 10:17:56 -0400

    Proyecto

---
+++ YourPiggyBank/YourPiggyBank/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using YourPiggyBank.ViewModels;
+using YourPiggyBank.Views;
+using Xamarin.Forms;
+
+namespace YourPiggyBank
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
+
